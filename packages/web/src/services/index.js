2dc3c393218c5731c813eae7f79bde15e867b882
sid.shi <18262272638@163.com>
2022-06-04 21:38:23 +0800

    支持数组形式，book 第一次获取成功后，第二次不会再去请求

--- packages/web/src/services/index.js
+++ packages/web/src/services/index.js
@@ -1,24 +1,53 @@
 
 const promiseCache = new Map()
 
+const getApiData = (url) => {
+  if (url === "/data") return Promise.reject(`get ${url} failed`)
+  return fetch(url).then(res => res.json())
+}
+
 const getBackData = async () => {
   const getData = async (url) => {
-    let promise = promiseCache.get(url)
+    const urlIsArray = Array.isArray(url)
+    // 统一化处理数据，无论是字符串还是数组均视为数组
+    const urls = urlIsArray ? url : [url]
 
-    if (!promise) {
-      // 没有数据请求服务器, 没有await 返回的是promise
-      promise = fetch(url).then(res => res.json())
-      // 设置数据缓存
-      promiseCache.set(url, promise)
-    }
-    return promise
+    // 获取所有的 请求服务
+    const promises = []
+
+    urls.forEach(url => {
+      // 利用promise 
+      let promise = promiseCache.get(url)
+
+      if (promise) {
+        // 如果 缓存中有，直接push
+        promises.push(promise)
+      } else {
+        promise = getApiData(url).catch(error => {
+          // 在请求回来后，如果出现问题，把promise从cache中删除
+          promiseCache.delete(url)
+          return Promise.reject(error)
+        })
+        promiseCache.set(url, promise)
+        promises.push(promise)
+      }
+    })
+
+    return Promise.all(promises).then(res => {
+      // 根据传入的 是字符串还是数组来返回数据，因为本身都是数组操作
+      // 如果传入的是字符串，则需要取出操作
+      return urlIsArray ? res : res[0]
+    })
   }
-  
-  const data1 = getData("/data")
-  const data2 = getData("/data")
 
-  const res = await Promise.all([data1, data2])
-  console.log(res)
+  const data1 = getData("/book")
+   // 第二次调用 不会去取 book，只会去data
+  const data2 = getData(["/data","/book"]) 
+
+  data1.then(console.log)
+  data2.then(console.log)
+
+  // const res = await Promise.all([data1, data2])
   // const data2 = await getData("/book")
   // console.log(data2)
 }
